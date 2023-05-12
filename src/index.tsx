41ec44185b22e6bdeecaf30af047339aeddc2b9a
Estiknok <95084542+Estiknok@users.noreply.github.com>
2023-05-12 22:47:44 +0200

    another try

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 
 const client = new ApolloClient({
-    uri: "https://46.136.24.155/graphql", //"http://localhost:4758/graphql",
+    uri: "http://46.136.24.155/graphql", //"http://localhost:4758/graphql",
     cache: new InMemoryCache(),
 });
 
